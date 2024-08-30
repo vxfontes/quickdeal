6c0e611a69860a1ec8e77305bc0f3e5e0535dea4
vxfontes <nessa1vane@icloud.com>
2024-08-29 22:14:46 -0300

    fix: removendo data

--- bff/src/shared/core/errors/bad-request.error.ts
+++ bff/src/shared/core/errors/bad-request.error.ts
@@ -6,7 +6,4 @@ export class BadRequestResponseDto {
 
     @ApiProperty({ example: "Houve um erro durante a requisição.", description: "Mensagem do erro." })
     message: string;
-
-    @ApiProperty({ example: "{ }", description: "Algum retorno do tipo de erro." })
-    data?: any;
 }
